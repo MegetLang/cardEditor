a89b0c066745398df74373332a1eeefa024002eb
MegetLang <lassefredheim@gmail.com>
2017-06-29 20:46:39 +0200

    SQL STUFF AND SHIT

---
+++ Database/Cards.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Cards]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Type] NCHAR(10) NULL, 
+    [Strength] TINYINT NULL, 
+    [Defense] TINYINT NULL, 
+    [Mana Cost] TINYINT NULL, 
+    [Artwork] NCHAR(10) NULL
+)
