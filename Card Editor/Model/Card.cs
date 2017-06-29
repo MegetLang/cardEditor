c6163940947ba8b7e96a1725e30de8b627c6775e
MegetLang <lassefredheim@gmail.com>
2017-06-29 17:43:28 +0200

    Initial commit

---
+++ Card Editor/Model/Card.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Editor.Model
+{
+    class Card
+    {
+        public string CardName { get; set; }
+
+        public string CardType { get; set; }
+
+        public int Strength { get; set; }
+        public int Defense { get; set; }
+        public int ManaCost { get; set; }
+
+        public Card()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            CardName = "";
+            CardType = "";
+            Strength = 1;
+            Defense = 1;
+            ManaCost = 1;
+        }
+        public void Save()
+        {
+            //TODO: Serialize with JSON
+        }
+        public void Load()
+        {
+            // TODO: Replace code here with something that deserializes (loads) our data in an appropriate format.
+            //       How about doing it with Json.NET, that we will learn about in a future lesson? :-)
+        }
+    }
+}
---
+++ Card Editor/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+using Card_Editor.Model;
+
+namespace Card_Editor.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+    }
+}
